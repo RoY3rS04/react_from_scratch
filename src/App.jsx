202ed10858d2895bb0ac4d0a81f565c4f060260c
RoY3rS04 <rogermoises2004@gmail.com>
2026-03-27 11:11:47 -0600

    Fundamentals section finished

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+let tech = "React, Tailwind and Vite";
+
+export function App() {
+    return (
+        <h1 className="font-mono">Hello, {tech}!</h1>
+    )
+}
---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import ReactDOM, { createRoot } from "react-dom/client";
+import "./app.css";
+import { StrictMode } from "react";
+import { App } from "./App";
+
+createRoot(document.getElementById('app')).render(
+    <StrictMode>
+        <App />
+    </StrictMode>
+);
